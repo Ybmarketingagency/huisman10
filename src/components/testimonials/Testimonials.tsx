9293d0e707b7915569ccb623132c50446e59713d
Ybmarketingagency <ybmarketingagencyprive@gmail.com>
2025-11-29 15:56:45 +0100

    Updated Testimonials.tsx

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Testimonials = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://ta-widget.netlify.app/widget-loader.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      const existingScript = document.querySelector('script[src="https://ta-widget.netlify.app/widget-loader.js"]');
+      if (existingScript) {
+        document.body.removeChild(existingScript);
+      }
+    };
+  }, []);
+
   return (
     <section id="testimonials" className="py-20 bg-[#d1d1d1]">
       <div className="container mx-auto px-4 md:px-6">
-        <div data-widget-id="b89266e8-73eb-4041-b8fd-e4fa791b0d9c"></div>
+        <div data-widget-id="eb344ec9-448b-454a-96de-43106de1ef80"></div>
       </div>
     </section>
   );
