48b573a9ec60ac486243f6a43accfef29ac8d554
Ybmarketingagency <ybmarketingagencyprive@gmail.com>
2025-11-29 20:42:58 +0100

    Updated Testimonials.tsx

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -18,7 +18,7 @@ const Testimonials = () => {
   return (
     <section id="testimonials" className="py-20 bg-[#d1d1d1]">
       <div className="container mx-auto px-4 md:px-6">
-        <div data-widget-id="85056126-f743-4275-9150-00800d217b18"></div>
+        <div data-widget-id="4af32e70-2399-4a3b-b0f0-ef5ea3c1ffb4"></div>
       </div>
     </section>
   );
