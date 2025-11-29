99dd6ca0499fad33ff39b44bf97f4b2262a8a6c9
Ybmarketingagency <ybmarketingagencyprive@gmail.com>
2025-11-29 15:47:49 +0100

    Updated index.html

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const Testimonials = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://ta-widget.netlify.app/widget-loader.js";
-    script.setAttribute('data-widget-id', 'be69b558-4d0c-4d1d-9e00-4fc00115e2e7');
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
     <section id="testimonials" className="py-20 bg-[#d1d1d1]">
       <div className="container mx-auto px-4 md:px-6">
-        <div id="google-reviews-widget-be69b558-4d0c-4d1d-9e00-4fc00115e2e7"></div>
+        <div data-widget-id="b89266e8-73eb-4041-b8fd-e4fa791b0d9c"></div>
       </div>
     </section>
   );
