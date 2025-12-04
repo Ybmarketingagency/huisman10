545073021ebe9785692bdf51f960124276e46e28
Ybmarketingagency <ybmarketingagencyprive@gmail.com>
2025-12-04 09:04:02 +0100

    Updated Testimonials.tsx

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -3,12 +3,12 @@ import React, { useEffect } from 'react';
 const Testimonials = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://ta-widget.bolt.host/widget-loader.js';
+    script.src = 'https://elfsightcdn.com/platform.js';
     script.async = true;
     document.body.appendChild(script);
 
     return () => {
-      const existingScript = document.querySelector('script[src="https://ta-widget.bolt.host/widget-loader.js"]');
+      const existingScript = document.querySelector('script[src="https://elfsightcdn.com/platform.js"]');
       if (existingScript) {
         document.body.removeChild(existingScript);
       }
@@ -18,7 +18,7 @@ const Testimonials = () => {
   return (
     <section id="testimonials" className="py-20 bg-[#d1d1d1]">
       <div className="container mx-auto px-4 md:px-6">
-        <div data-widget-id="bf45970f-5d5f-4dc8-821f-7196974ac923"></div>
+        <div className="elfsight-app-42e45f1b-bc33-435d-a2a8-d98c1cb4c564" data-elfsight-app-lazy></div>
       </div>
     </section>
   );
