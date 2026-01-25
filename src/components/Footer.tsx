d63eb31a74b652cfdc01fb13424ee9b6ffbe98ba
Ybmarketingagency <ybmarketingagencyprive@gmail.com>
2026-01-25 11:38:17 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,7 +16,7 @@ const Footer = () => {
           <div>
             <div className="flex items-center mb-4">
               <img
-                src="https://i.imgur.com/N8OCXeL.png"
+                src="https://i.imgur.com/1B18QpN.png"
                 alt="Behang & Schilderservice"
                 className="h-16"
               />
